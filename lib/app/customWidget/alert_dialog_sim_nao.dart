abf6ffc708b919304485931c5d794ee1e11f24e4
Andre Lucyo <andre.lucyo2@gmail.com>
2021-06-06 01:01:57 -0300

    Ajuste menu home, inico alertDialog

---
+++ lib/app/customWidget/alert_dialog_sim_nao.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+class AlertDialogSimNao extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return AlertDialog();
+  }
+}
--- lib/app/customWidget/drawer_home.dart
+++ lib/app/customWidget/drawer_home.dart
@@ -1,35 +1,54 @@
 import 'package:estudo_flutter_ui_02/app/customWidget/swtch_theme.dart';
+import 'package:estudo_flutter_ui_02/app/routes/app_routes.dart';
 import 'package:flutter/material.dart';
 
 class DrawerHome extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Drawer(
-      child: Column(
+      child: ListView(
         children: [
           UserAccountsDrawerHeader(
-              currentAccountPicture: ClipOval(
-                child: Image.asset(
-                  'assets/images/userAvatar.png',
-                  fit: BoxFit.cover,
-                ),
+            accountName: Text('Andre Lucio da Silva'),
+            accountEmail: Text('andre.lucyo2@gmail.com'),
+            currentAccountPicture: ClipOval(
+              child: Image.asset(
+                'assets/images/userAvatar.png',
+                fit: BoxFit.cover,
               ),
-              accountName: Text('Andre Lucio da Silva'),
-              accountEmail: Text('andre.lucyo2@gmail.com')),
+            ),
+            otherAccountsPictures: [
+              CircleAvatar(
+                child: Text('Als'),
+                backgroundColor: Colors.white60,
+              ),
+              CircleAvatar(
+                child: Text('123'),
+              ),
+            ],
+            onDetailsPressed: () {
+              print('Detlhes do User.');
+            },
+          ),
           ListTile(
             leading: Icon(Icons.home),
             title: Text('Home'),
             subtitle: Text('Tela de inicio.'),
             onTap: () {
+              Navigator.of(context).pushReplacementNamed(AppRoutes.HOME);
               print('Tela home');
             },
           ),
+          Divider(),
           ListTile(
-            leading: Icon(Icons.logout),
-            title: Text('Sair'),
-            subtitle: Text('Encerrar sessão.'),
+            leading: Icon(Icons.person),
+            title: Text('Profile'),
+            subtitle: Text('Perfil de usuario.'),
+            onLongPress: () {
+              print('Long ... Tela editar Profile');
+            },
             onTap: () {
-              Navigator.of(context).pushReplacementNamed('/');
+              print('Tela editar Profile');
             },
           ),
           ListTile(
@@ -40,12 +59,35 @@ class DrawerHome extends StatelessWidget {
               print('Abre a tela configurações');
             },
           ),
+          Divider(),
+          ListTile(
+            title: Text('Calls'),
+            leading: Icon(Icons.call),
+            onLongPress: () {},
+          ),
+          ListTile(
+            title: Text('Support'),
+            leading: Icon(
+              Icons.report_problem,
+              color: Colors.red,
+            ),
+            onLongPress: () {},
+          ),
           ListTile(
             leading: SwtchTheme(),
             title: Text('Config. Tema'),
             subtitle: Text('Tema Dark ou ligth'),
             onTap: () {
               print('Abre a tela configurações');
+            },
+          ),
+          Divider(),
+          ListTile(
+            leading: Icon(Icons.logout),
+            title: Text('Sair'),
+            subtitle: Text('Encerrar sessão.'),
+            onTap: () {
+              Navigator.of(context).pushReplacementNamed(AppRoutes.MAIN);
             },
           ),
         ],
