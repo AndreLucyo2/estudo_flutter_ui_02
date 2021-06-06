f6ad5d74ae19545d50b91c9a64f70dad68b08530
Andre Lucyo <andre.lucyo2@gmail.com>
2021-06-05 23:40:43 -0300

    Criado padrao de projeto

---
+++ lib/app/customWidget/drawer_home.dart
@@ -0,0 +1,54 @@
+import 'package:estudo_flutter_ui_02/app/customWidget/swtch_theme.dart';
+import 'package:flutter/material.dart';
+
+class DrawerHome extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Drawer(
+      child: Column(
+        children: [
+          UserAccountsDrawerHeader(
+              currentAccountPicture: ClipOval(
+                child: Image.asset(
+                  'assets/images/userAvatar.png',
+                  fit: BoxFit.cover,
+                ),
+              ),
+              accountName: Text('Andre Lucio da Silva'),
+              accountEmail: Text('andre.lucyo2@gmail.com')),
+          ListTile(
+            leading: Icon(Icons.home),
+            title: Text('Home'),
+            subtitle: Text('Tela de inicio.'),
+            onTap: () {
+              print('Tela home');
+            },
+          ),
+          ListTile(
+            leading: Icon(Icons.logout),
+            title: Text('Sair'),
+            subtitle: Text('Encerrar sessão.'),
+            onTap: () {
+              Navigator.of(context).pushReplacementNamed('/');
+            },
+          ),
+          ListTile(
+            leading: Icon(Icons.settings),
+            title: Text('Configurações'),
+            subtitle: Text('Tela de configurações.'),
+            onTap: () {
+              print('Abre a tela configurações');
+            },
+          ),
+          Column(
+            mainAxisAlignment: MainAxisAlignment.end,
+            children: [
+              Text('Tema Dark ou ligth.'),
+              SwtchTheme(),
+            ],
+          ),
+        ],
+      ),
+    );
+  }
+}
