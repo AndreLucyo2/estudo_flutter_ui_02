625c624551c17f495d55fd72ddd76b72e38d7838
Andre Lucyo <andre.lucyo2@gmail.com>
2021-06-07 20:43:25 -0300

    inicio do CRUD estudo SqlIte

---
+++ lib/app/model/entidades/user.dart
@@ -0,0 +1,9 @@
+class User {
+  final int id;
+  final String nome;
+  final String email;
+  final String senha;
+  final String hashUser;
+
+  User(this.id, this.nome, this.email, this.senha, this.hashUser);
+}
